3335a401b6765eeaa9e4f4352ee13dabc5bf86e4
James Reinhart <jreinhart@orscheln.com>
2020-02-20 14:15:36 -0600

    changed updater to try it

--- main.js
+++ main.js
@@ -1,53 +1,58 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const { app, BrowserWindow, ipcMain } = require("electron");
+// const { autoUpdater } = require("electron-updater");
+require("update-electron-app")({
+    repo: "jamesreinhart90/electron-auto-update-example",
+    updateInterval: "1 hour",
+    notifyUser: false
+});
 
 let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-  mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadFile("index.html");
+    mainWindow.on("closed", function() {
+        mainWindow = null;
+    });
+    // mainWindow.once('ready-to-show', () => {
+    //   autoUpdater.checkForUpdatesAndNotify();
+    // });
 }
 
-app.on('ready', () => {
-  createWindow();
-});
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', function () {
-  if (mainWindow === null) {
+app.on("ready", () => {
     createWindow();
-  }
 });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
+app.on("window-all-closed", function() {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+app.on("activate", function() {
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+ipcMain.on("app_version", event => {
+    event.sender.send("app_version", { version: app.getVersion() });
 });
 
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
+// autoUpdater.on('update-available', () => {
+//   mainWindow.webContents.send('update_available');
+// });
+
+// autoUpdater.on('update-downloaded', () => {
+//   mainWindow.webContents.send('update_downloaded');
+// });
+
+// ipcMain.on('restart_app', () => {
+//   autoUpdater.quitAndInstall();
+// });
